e0c63651b87383abdfcc4886d00c73439f61d3d1
Scott Ricketts <SJR@imac.lan>
2020-09-15 13:01:32 -0500

    Add function for filtered table to appear

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -24,12 +24,14 @@ var button = d3.select("#filter-btn");
 // QUESTION: is this the corect id???
 var form = d3.select("#form-group");
 
-// Create event handlers for both clicking the button or hitting enter on the keyboard 
+// Create event handlers for both clicking the button or hitting enter on the keyboard
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
 // Event handler function to sort by date
 function runEnter() {
+  // Clear the table
+  tbody.html("");
 
   // Prevent the page from refreshing
   d3.event.preventDefault();
@@ -42,5 +44,14 @@ function runEnter() {
 
   // Use the form input to filter the data by date
   var filteredData = tableData.filter((data) => data.datetime === inputValue);
+
+  // Table of filtered data appears after event
+  filteredData.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
   console.log(filteredData);
-};
+}
